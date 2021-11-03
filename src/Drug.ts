4bb48481c359cfe9c642023f2c74298818c6971b
TMN <t.martin@adsoftware.fr>
2021-11-03 14:50:00 +0100

    chore: jsdoc

--- src/Drug.ts
+++ src/Drug.ts
@@ -1,14 +1,48 @@
-export type DrugName = "Herbal Tea" | "Fervex" | "Magic Pill" | "Doliprane";
-
+export type DrugName =
+  | "Herbal Tea"
+  | "Fervex"
+  | "Magic Pill"
+  | "Doliprane"
+  | "Dafalgan";
+/**
+ * Represents a Drug
+ 
+ * @param {expiresIn} number - The umber of days until the drug expires.
+ * @param {benefit} number - Value which denotes how powerful the drug is
+ * @param {decreaseBenefitRate} number - The rate at which the benefit decreases everyday
+ * @param {benefitLimit} number - The limit this drug's benefit can't top
+ */
 export class Drug {
   private readonly _name: DrugName;
   private _expiresIn: number;
   private _benefit: number;
-  constructor(name: DrugName, expiresIn: number, benefit: number) {
+  private _decreaseBenefitRate: number;
+  private _benefitLimit: number;
+  constructor(
+    name: DrugName,
+    expiresIn: number,
+    benefit: number,
+    decreaseBenefitRate = 1,
+    // The Benefit of an item is never more than 50
+    benefitLimit = 50
+  ) {
     this._name = name;
     this._expiresIn = expiresIn;
     this._benefit = benefit;
+    this._decreaseBenefitRate = decreaseBenefitRate;
+    this._benefitLimit = benefitLimit;
   }
+
+  /**
+   * Update the value of the drug expiresIn property by one day if the conditions are met
+   * @returns void
+   */
+  public updateExpiresIn(): void {
+    //"Magic Pill" never expires
+    if (this.name === "Magic Pill") return;
+    this.expiresIn--;
+  }
+
   public get name(): DrugName {
     return this._name;
   }
@@ -16,41 +50,52 @@ export class Drug {
     return this._expiresIn;
   }
 
-  public updateBenefit() {
+  /**
+   * Update  the value of the drug benefit if the conditions are met
+   * @returns void
+   */
+  public updateBenefit(): void {
     switch (this.name) {
       case "Fervex":
         switch (true) {
-          case this.benefit >= 50:
+          //Benefit drops to 0 after the expiration date
+          case this.expiresIn < 0:
+            this.benefit = 0;
             return;
-          case this.expiresIn <= 0:
-            return (this.benefit = 0);
+          //Benefit increases by 3 when there are 5 days or less
           case this.expiresIn <= 5:
-            return (this.benefit += 3);
+            this.benefit += 3;
+            return;
+          //Benefit increases by 2 when there are 10 days
           case this.expiresIn <= 10:
-            return (this.benefit += 2);
-
+            this.benefit += 2;
+            return;
           default:
             this.benefit++;
             break;
         }
       case "Herbal Tea":
+        // Herbal Tea" actually increases in Benefit the older it gets
         switch (true) {
-          case this.benefit >= 50:
-            return;
+          //Benefit increases twice as fast after the expiration date
           case this.expiresIn < 0:
-            return (this.benefit += 2);
+            this.benefit += this._decreaseBenefitRate * 2;
+            return;
           default:
-            return this.benefit++;
+            this.benefit += this._decreaseBenefitRate;
+            return;
         }
-
+      case "Magic Pill":
+        //"Magic Pill" never decreases in Benefit
+        return;
       default:
+        // Once the expiration date has passed, Benefit degrades twice as fast
+        if (this._expiresIn === -1) {
+          this._decreaseBenefitRate = this._decreaseBenefitRate * 2;
+        }
+        this.benefit -= this._decreaseBenefitRate;
         return;
     }
-  }
-
-  public updateExpiresIn() {
-    if (this.name === "Magic Pill") return;
-    this.expiresIn--;
   }
 
   public set expiresIn(expiresIn: number) {
@@ -62,6 +107,18 @@ export class Drug {
   }
 
   public set benefit(benefit: number) {
+    // Benefit won't update if the limit is reached
+    if (benefit >= this._benefitLimit) {
+      this._benefit = this._benefitLimit;
+      return;
+    }
+
+    // The Benefit of an item is never negative
+    if (benefit < 0) {
+      this._benefit = 0;
+      return;
+    }
+
     this._benefit = benefit;
   }
 }
--- src/pharmacy.ts
+++ src/pharmacy.ts
@@ -1,9 +1,17 @@
 import { Drug } from "./Drug";
 
+/**
+ * Represents a pharmacy
+ * @param {Drug[]} drugs - The list of drugs included in this pharmacy
+ */
 export class Pharmacy {
   constructor(public drugs: Drug[] = []) {}
 
-  updateBenefitValue() {
+  /**
+   * Updates the benefit and expiresIn values of every drug in the pharmacy
+   * @returns Drug[]
+   */
+  updateBenefitValue(): Drug[] {
     this.drugs.forEach(drug => {
       drug.updateExpiresIn();
       drug.updateBenefit();
